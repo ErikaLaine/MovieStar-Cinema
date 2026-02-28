67e150c89e62c35ce77fe3004332d1b7290f57d1
Eveliina Ketola <eveliina.ketola@elisanet.fi>
2026-02-28 16:52:29 +0200

    Update profiili.php

--- profiili.php
+++ profiili.php
@@ -1,17 +1,3 @@
-<?php
-// TIETOKANTAYHTEYS
-$servername = "localhost";
-$username   = "amk1013231";
-$password   = "IxNzc6lJ";
-$dbname     = "wp_amk1013231";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Tarkista yhteys
-if ($conn->connect_error) {
-    die("Yhteys epäonnistui: " . $conn->connect_error);
-}
-?>
 
 <?php
 require_once "db.php";
