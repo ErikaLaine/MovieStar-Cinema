d0fb17be54fd9ea880f1ecad9b3bf3455b528096
ErikaLaine <erika.laine@student.hamk.fi>
2026-03-03 13:08:12 +0200

    Update hae.php

--- hae.php
+++ hae.php
@@ -1,7 +1,9 @@
 <?php
 require_once "db.php";
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
+if ($conn->connect_error) {
+    die("Tietokantayhteys epäonnistui: " . $conn->connect_error);
+}
 
 $hakusana = "";
 
